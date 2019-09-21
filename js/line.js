c30dcd53933ebe1ef473b13a3d742168352a1d42
mmecius <tmecius@gmail.com>
2019-09-21 20:16:16 +0300

    add third html file

--- js/line.js
+++ js/line.js
@@ -9,9 +9,6 @@ function populateList(clients = [], clientsList) {
       return `
         <div class="flex">
         <li>
-          <input type="checkbox" data-index=${i} id="item${i}" ${
-        client.done ? "checked" : ""
-      } />
           <label for="item${i}">${client.clientName}</label>
         </li>
         <li>
@@ -23,23 +20,4 @@ function populateList(clients = [], clientsList) {
     .join("");
 }
 
-function toggleDone(e) {
-  const el = e.target;
-  const index = el.dataset.index;
-  items[index].done = !items[index].done;
-  localStorage.setItem("items", JSON.stringify(items));
-  populateList(items, itemsList);
-}
-
-function removeClient(e) {
-  const el = e.target;
-  const index = el.dataset.index;
-  document.getElementById("button").onclick = function () {
-    items.splice(index, 1);
-    localStorage.setItem("items", JSON.stringify(items));
-  };
-}
-
-itemsList.addEventListener("click", toggleDone);
-itemsList.addEventListener("click", removeClient);
 populateList(items, itemsList);
