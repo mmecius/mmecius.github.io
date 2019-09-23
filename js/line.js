57b72a02742182cc7e2151090fba9516f81c7bb7
mmecius <tmecius@gmail.com>
2019-09-23 18:59:33 +0300

    add JSON file, fix html and css

--- js/line.js
+++ js/line.js
@@ -1,3 +1,25 @@
+const dummyItemsList = document.querySelector(".dummyClients");
+
+var request = new XMLHttpRequest();
+request.open("GET", "https://api.myjson.com/bins/xyvkp", true);
+request.onreadystatechange = function () {
+  if (request.readyState != 4 || request.status != 200) return;
+  var dummyClients = JSON.parse(request.responseText);
+  for (i = 0; i < dummyClients.length; i++) {
+    dummyItemsList.innerHTML += `
+        <div class="flex">
+        <li>
+          <label for="item${i}">${dummyClients[i].clientName}</label>
+        </li>
+        <li>
+          <label for="item${i}">${dummyClients[i].specialistName}</label>
+        </li>
+        </div>
+      `;
+  }
+};
+request.send();
+
 const itemsList = document.querySelector(".clients");
 
 const items = JSON.parse(localStorage.getItem("items")) || [];
--- js/specialist.js
+++ js/specialist.js
@@ -1,11 +1,36 @@
+const dummyItemsList = document.querySelector(".dummyClients");
+
+var request = new XMLHttpRequest();
+request.open("GET", "https://api.myjson.com/bins/xyvkp", true);
+request.onreadystatechange = function () {
+  if (request.readyState != 4 || request.status != 200) return;
+  var dummyClients = JSON.parse(request.responseText);
+  for (i = 0; i < dummyClients.length; i++) {
+    dummyItemsList.innerHTML += `
+        <div class="flex">
+        <li>
+        <input type="checkbox" data-index=${i} id="item${i}" ${
+      dummyClients.done ? "checked" : ""
+    } />
+          <label for="item${i}">${dummyClients[i].clientName}</label>
+        </li>
+        <li>
+          <label for="item${i}">${dummyClients[i].specialistName}</label>
+        </li>
+        </div>
+      `;
+  }
+};
+request.send();
+
 const itemsList = document.querySelector(".clients");
 
 const items = JSON.parse(localStorage.getItem("items")) || [];
 
 function populateList(clients = [], clientsList) {
-    clientsList.innerHTML = clients
-        .map((client, i) => {
-            return `
+  clientsList.innerHTML = clients
+    .map((client, i) => {
+      return `
         <div class="flex">
         <li>
           <input type="checkbox" data-index=${i} id="item${i}" ${
@@ -18,27 +43,19 @@ function populateList(clients = [], clientsList) {
         </li>
         </div>
       `;
-        })
-        .join("");
-}
-
-function toggleDone(e) {
-    const el = e.target;
-    const index = el.dataset.index;
-    items[index].done = !items[index].done;
-    localStorage.setItem("items", JSON.stringify(items));
-    populateList(items, itemsList);
+    })
+    .join("");
 }
 
 function removeClient(e) {
-    const el = e.target;
-    const index = el.dataset.index;
-    document.getElementById("button").onclick = function () {
-        items.splice(index, 1);
-        localStorage.setItem("items", JSON.stringify(items));
-    };
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  document.getElementById("button").onclick = function () {
+    items.splice(index, 1);
+    localStorage.setItem("items", JSON.stringify(items));
+  };
 }
 
-itemsList.addEventListener("click", toggleDone);
 itemsList.addEventListener("click", removeClient);
 populateList(items, itemsList);
